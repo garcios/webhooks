6f94720001a3841062c35636a8b12607ca0828a0
a1633661 <oscarsalem.garcia@student.adelaide.edu.au>
2017-07-24 11:01:07 +0800

    added template engine

--- README.md
+++ README.md
@@ -6,3 +6,14 @@ npm install express --save
 npm install body-parser --save
 
 npm install socket.io --save
+
+npm install mustache --save
+
+npm install consolidate --save
+
+
+How to run:
+
+node app.js 3000 app-A
+
+node app.js 3100 app-B
--- app.js
+++ app.js
@@ -2,13 +2,20 @@
 
 const express = require('express');
 const bodyParser = require('body-parser')
-
+const cons = require('consolidate')
 
 const app = express();
 
+console.log(process.argv);
+
+var port = process.argv[2] || 3000;
+var appName = process.argv[3] || "app-A";
+console.log("appName:" + appName);
+
+
 // Run server to listen on port 3000.
-const server = app.listen(3000, () => {
-  console.log('listening on *:3000');
+const server = app.listen(port, () => {
+  console.log('listening on *:' + port);
 });
 
 const io = require('socket.io')(server);
@@ -18,6 +25,12 @@ app.use(bodyParser.urlencoded({ extended: false } ));
 app.use(express.static('static'));
 // parse application/json
 app.use(bodyParser.json());
+//assign the mustache engine to .html files
+app.engine('html', cons.mustache);
+//set .html as default extension
+app.set("view engine", 'html');
+app.set("views", __dirname + '/views');
+ 
  
 // Set socket.io listeners.
 io.on('connection', (socket) => {
@@ -30,7 +43,9 @@ io.on('connection', (socket) => {
  
 // Set Express routes.
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html');
+  //res.sendFile(__dirname + '/views/index.html');
+  var viewData = {'appName': appName};
+  res.render('index',viewData);
 });
 
 
